3c8d76736c4f340f5ba3dad9ae43b8cb7771ef01
ArkyvIO <arkyvio@gmail.com>
2022-02-17 17:25:51 -0500

    Server IP Change
    VPS Upgraded - New Server IP - Needed new IP in app files to run off server instead of local host.

--- citc/platforms/android/app/src/main/assets/www/js/services_courses.js
+++ citc/platforms/android/app/src/main/assets/www/js/services_courses.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('CITC.services', ['ngResource'])
-.constant("baseURL","http://10.0.2.2:3000/")
+.constant("baseURL","http://104.225.208.168:3000/")
 
 .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
